0ae701b0a49758c12668a6dad50cd8784f8f61cc
YukiHaze <gabriel.reinoso.1996@gmail.com>
2021-08-19 18:56:46 -0400

    0.1 source code

--- GDataTool/MainForm.Designer.cs
+++ GDataTool/MainForm.Designer.cs
@@ -29,6 +29,7 @@ namespace GDataTool
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(weaponForm));
             this.lstWeapons = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -669,6 +670,7 @@ namespace GDataTool
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(662, 531);
